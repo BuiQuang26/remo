2a4a4c8e6b3fcf808095c62a487ec557bf382b7d
Qaker-VN <85881101+Qaker-VN@users.noreply.github.com>
2022-02-16 23:24:48 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const connect = require('./config/db');
 var methodOverride = require('method-override');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 app.use(methodOverride('_method'));
 
